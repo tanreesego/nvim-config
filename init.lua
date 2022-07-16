e337c1c63136b522c4a3db24ec3aa658b994030b
tanreesego <tanreesego2@gmail.com>
2022-07-16 23:17:51 +0800

    feat: live-grep telescope, nvim-tree update config, added autopairs, attempted to add autotags but nah

--- init.lua
+++ init.lua
@@ -1,7 +1,8 @@
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.cmp"
-require "user.lsp"
-require "user.treesitter" 
-require "user.nvim-tree" 
+require("user.options")
+require("user.keymaps")
+require("user.plugins")
+require("user.cmp")
+require("user.lsp")
+require("user.treesitter")
+require("user.nvim-tree")
+require("user.autopairs")
